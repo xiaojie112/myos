2e53d241db2512c2cd6545c1027bd8f383a8ed2a
ziya <644735414@qq.com>
2023-03-26 10:27:02 -0700

    输出hello world!

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include "tools/floppy.h"
+#include "tools/fileinfo.h"
 
 int main() {
-    printf("Hello, World!\n");
+    char* boot_filepath = "/home/ziya/CLionProjects/myos/build/boot/boot.o";
+
+    Floppy* floppy = create_floppy();
+
+    Fileinfo* boot_fileinfo = read_file(boot_filepath);
+    write_bootloader(floppy, boot_fileinfo);
+
+    create_image("/home/ziya/CLionProjects/myos/a.img", floppy);
+
     return 0;
 }
